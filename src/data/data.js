2a35b6134f401fc057580418e61539b00d5d09da
Abeloth19 <prajwalmahajan258@gmail.com>
2023-05-10 11:35:08 +0530

    image and icon changes

--- src/data/data.js
+++ src/data/data.js
@@ -42,7 +42,8 @@ export const data = [
       category: 'veg',
       image:
         'https://media.istockphoto.com/id/1273066287/photo/image-of-cast-iron-round-skillet-frying-pan-vegetarian-meal-palak-paneer-recipe-blue-grey.jpg?b=1&s=170667a&w=0&k=20&c=Mi-s2JV4SO0eyk9p0Zp5x92ZoiXCEzF2xymxn8e6ROA=',
-      price: '$$',
+        price: '300 ₹',
+        prices : '200-500',
     },
     {
       id: 6,
@@ -50,7 +51,8 @@ export const data = [
       category: 'veg',
       image:
         'https://media.istockphoto.com/id/1385710961/photo/healthy-vegetable-and-brown-rice-stir-fry.jpg?b=1&s=170667a&w=0&k=20&c=bGTNM4b_4ozAR5uaVTlxSFxZEdEwpA2xnnTavv4CG6A=',
-      price: '$$$',
+        price: '375 ₹',
+        prices : '200-500',
     },
     {
       id: 7,
@@ -58,7 +60,8 @@ export const data = [
       category: 'veg',
       image:
         'https://media.istockphoto.com/id/1412464690/photo/caprese-salad-with-mozzarella-tomatoes-and-basil.jpg?b=1&s=170667a&w=0&k=20&c=R-frT_JSf7ei0mGz6Zl1kbHshLA0ZpJN7lXs6tjJ4Kc=',
-      price: '$$$$',
+        price: '400 ₹',
+        prices : '200-500',
     },
     {
       id: 8,
@@ -66,122 +69,131 @@ export const data = [
       category: 'veg',
       image:
         'https://media.istockphoto.com/id/1392104124/photo/woman-eating-falafel-in-a-restaurant.jpg?b=1&s=170667a&w=0&k=20&c=UCt9vrMMkRePsGOVEV0bFWM_YDwBMp9UvnX-ZYuyx2c=',
-      price: '$',
+        price: '275 ₹',
+        prices : '200-500',
     },
     {
       id: 9,
-      name: 'Kale Salad',
-      category: 'salad',
+      name: 'Butter Chicken',
+      category: 'non-veg',
       image:
-        'https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$',
+        'https://media.istockphoto.com/id/1373284422/photo/indian-creamy-butter-chicken-curry-with-paratha-bread-served-in-copper-bowl.jpg?b=1&s=170667a&w=0&k=20&c=-a2xzpgIy2fTbVYpcGHJK9hvvRBHEJ4gbzaoicKJxrY=',
+        price: '380 ₹',
+        prices : '200-500',
     },
     {
       id: 10,
-      name: 'Ceasar Salad',
-      category: 'salad',
+      name: 'Fish and Chips',
+      category: 'non-veg',
       image:
-        'https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$$',
+        'https://media.istockphoto.com/id/1370199987/photo/gourmet-fish-and-chips.jpg?b=1&s=170667a&w=0&k=20&c=wb-jOr6fNzqYXgJg2wdoCUP6b5FDbgPCZbXM_H_yQGA=',
+        price: '510 ₹',
+        prices : '500+',
     },
     {
       id: 11,
-      name: 'Loaded Salad',
-      category: 'salad',
+      name: 'Chicken Kabab',
+      category: 'non-veg',
       image:
-        'https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$$$',
+        'https://media.istockphoto.com/id/912629972/photo/chicken-kebab-with-bell-pepper.jpg?b=1&s=170667a&w=0&k=20&c=7ocAfaN3z8nrH8Fr7KpIwMrHej8IABRK7U7s-xPhIKw=',
+        price: '480 ₹',
+        prices : '200-500',
     },
     {
       id: 12,
-      name: 'Fruit Salad',
-      category: 'salad',
+      name: 'Sushi',
+      category: 'non-veg',
       image:
-        'https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-      price: '$',
+        'https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3VzaGl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        price: '520 ₹',
+        prices : '500+',
     },
     {
       id: 13,
-      name: 'Wings',
-      category: 'chicken',
+      name: 'Cheesecake',
+      category: 'Dessert',
       image:
-        'https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$',
+        'https://media.istockphoto.com/id/1352497199/photo/slice-of-cheesecake-with-chocolate-sauce.jpg?b=1&s=170667a&w=0&k=20&c=ktA4zsk54NrsJ_KngCMmyTiRTEuyXGF-KC--42pNmhU=',
+        price: '575 ₹',
+        prices : '500+',
     },
     {
       id: 14,
-      name: 'Baked Chicken',
-      category: 'chicken',
+      name: 'Gulab Jamun',
+      category: 'Dessert',
       image:
-        'https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$$$',
+        'https://media.istockphoto.com/id/521803129/photo/gulab-jamun-11.jpg?b=1&s=170667a&w=0&k=20&c=Ir8N51FhWmAN_bQNKiscp0wvPgju9yaqN3Lj9-13NSQ=',
+        price: '105 ₹',
+        prices : '100-200',
     },
     {
       id: 15,
-      name: 'Chicken Tenders',
-      category: 'chicken',
+      name: 'Tiramisu',
+      category: 'Dessert',
       image:
-        'https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      price: '$',
+        'https://media.istockphoto.com/id/517368976/photo/slice-of-dessert.jpg?b=1&s=170667a&w=0&k=20&c=4H-Ejr0quaVSBu_vAt7mfK6ipFmYUUGG_ecDrhdNCg0=',
+        price: '370₹',
+        prices : '200-500',
     },
     {
       id: 16,
-      name: 'Chicken Kabob',
-      category: 'chicken',
+      name: 'Crème Brûlée',
+      category: 'Dessert',
       image:
-        'https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      price: '$$$',
+        'https://plus.unsplash.com/premium_photo-1667808384851-8f041275a8c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y3JlbWUlMjBicnVsZWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        price: '600 ₹',
+        prices : '500+',
     },
   ];
   
   export const categories = [
     {
       id: 1,
-      name: 'Fast Food',
+      name: 'Japanese',
       image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/FastFood_BrowseHome@3x.png',
+        'https://cdn.iconscout.com/icon/premium/png-256-thumb/sushi-200-778804.png?f=avif&w=128',
     },
     {
       id: 2,
-      name: 'Pizza',
+      name: 'French',
       image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Pizza_BrowseHome@3x.png',
+        'https://cdn.iconscout.com/icon/premium/png-256-thumb/ratatouille-8100895-6463822.png?f=avif&w=128',
     },
     {
       id: 3,
-      name: 'Wings',
+      name: 'American',
       image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Wings_BrowseHome@3x.png',
+        'https://cdn.iconscout.com/icon/premium/png-128-thumb/burger-7-136282.png',
     },
     {
       id: 4,
       name: 'Indian',
       image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Indian_BrowseHome@3x.png',
+        'https://cdn.iconscout.com/icon/free/png-256/free-curry-rice-lunch-dish-food-emoj-symbol-30694.png?f=avif&w=128',
     },
     {
       id: 5,
-      name: 'Latest Deals',
+      name: 'Mexican',
       image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png',
+        'https://cdn.iconscout.com/icon/premium/png-256-thumb/tacos-19-677811.png?f=avif&w=128',
     },
     {
       id: 6,
-      name: 'Restaurant Rewards',
+      name: 'Spanish',
       image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png',
+        'https://cdn.iconscout.com/icon/premium/png-256-thumb/tapas-8049583-6404434.png?f=avif&w=128',
     },
     {
       id: 7,
-      name: 'Best Overall',
+      name: 'Italian',
       image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png',
+        'https://cdn.iconscout.com/icon/free/png-256/free-pizza-257-450350.png?f=avif&w=128',
     },
     {
       id: 8,
-      name: 'Shipped Free',
+      name: 'korean',
       image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
+        'https://cdn.iconscout.com/icon/premium/png-256-thumb/bibimbap-3567126-2982203.png?f=avif&w=128',
     },
   ];
   
